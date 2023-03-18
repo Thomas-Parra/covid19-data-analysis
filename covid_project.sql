673a1f2d185df2e9643c13210f48f143d099d067
Thomas Parra <tparraa@outlook.com>
2023-03-18 16:10:54 -0500

    Query added

--- covid_project.sql
+++ covid_project.sql
@@ -24,3 +24,36 @@ FROM DEATHS
 GROUP BY 1,3
 HAVING MAX((TOTAL_CASES/POPULATION)*100) IS NOT NULL
 ORDER BY 4 DESC;
+
+
+-- Countries with highest death counts
+
+SELECT LOCATION, MAX(TOTAL_DEATHS) AS DEATHS_COUNT
+FROM DEATHS
+WHERE CONTINENT IS NOT NULL
+GROUP BY 1
+HAVING MAX(TOTAL_DEATHS) IS NOT NULL
+ORDER BY 2 DESC;
+
+
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Showing contintents with the highest death count per population
+
+Select continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Where continent is not null 
+Group by continent
+order by TotalDeathCount desc
+
+
+
+-- GLOBAL NUMBERS
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
